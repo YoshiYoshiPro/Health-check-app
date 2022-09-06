66191d7e4384112258908ebb534204b238918097
toy-kshr <toy.kasahara@gmail.com>
2022-09-06 18:17:53 +0000

    ddd, Sep 9/6/2022, YYYY, 6:17 A +0

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required
+from helpers import apology, login_required, admin_required
 
 app = Flask(__name__)
 
@@ -262,6 +262,7 @@ def adminreg():
 
 # 管理者ID表示
 @app.route("/adminid")
+@admin_required
 def adminid():
 
     # データベース接続
@@ -276,6 +277,7 @@ def adminid():
 
 # 管理ページ
 @app.route("/adminhome")
+@admin_required
 def adminhome():
 
     return render_template("adminhome.html")
