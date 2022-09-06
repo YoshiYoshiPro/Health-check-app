a8663ff7dc9cfecad3a39a7aad3faa5b9bde2b7a
toy-kshr <toy.kasahara@gmail.com>
2022-09-06 18:16:37 +0000

    ddd, Sep 9/6/2022, YYYY, 6:16 A +0

--- helpers.py
+++ helpers.py
@@ -29,3 +29,16 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def admin_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("group_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
