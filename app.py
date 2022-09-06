3a7da849202a9028b98321230b2a617604282df3
toy-kshr <toy.kasahara@gmail.com>
2022-09-06 13:55:13 +0000

    ddd, Sep 9/6/2022, YYYY, 1:55 A +0

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def login():
         conn = sqlite3.connect("health.db")
         db = conn.cursor()
          # データベースにユーザー名を問い合わせる
-        rows = db.execute(sql, (userid, ).fetchone())
+        rows = db.execute(sql, (userid, )).fetchone()
 
         # ユーザー名が存在し、次はパスワードが正しいか確認する。
         if rows[0]["user_id"] != userid or not check_password_hash(rows[0]["hash"], request.form.get("password")):
