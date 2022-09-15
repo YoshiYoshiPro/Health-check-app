77069955080be302c8414f2aadfdcdc79896bffa
DESKTOP-GID09QK\yoshi <yoshi.p30131@icloud.com>
2022-09-15 10:51:52 +0900

    全角スペース修正

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def adminlogin():
         elif not request.form.get("password"):
             return apology("adminlogin.html", "パスワードを入力してください")
 
-        # データベース接続処理　CS50を使わないバージョン
+        # データベース接続処理 CS50を使わないバージョン
         conn = sqlite3.connect("health.db")
         conn.row_factory = dict_factory
         cur = conn.cursor()
