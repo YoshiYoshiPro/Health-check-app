47b0eccad9acf1fd09e62451806fcf41478a3af3
DESKTOP-GID09QK\yoshi <yoshi.p30131@icloud.com>
2022-09-20 18:04:44 +0900

    [add] IDのコピーボタン作成

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
-function copy() {
-    var text = document.getElementById("copyTarget");
-    text.select();
-    document.execCommand("Copy");
-}
+// クリップボードにIDを保存
+var clipboard = new ClipboardJS('.copy');
+// クリップ成功
+clipboard.on('success', (e) => {
+    const title = e.trigger.dataset.clipboardTitle;
+    alert(title + 'をコピーしたよ');
+});
